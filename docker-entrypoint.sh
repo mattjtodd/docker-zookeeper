e0490873a2d9ac1689da90faa60a69ce1eaec1d9
Matt Todd <matt.todd@blackcatsolutions.co.uk>
2016-09-02 13:51:25 +0100

    More flexible setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,24 +2,17 @@
 
 set -e
 
-# check to see if the first argument is a parameter, add command
-if [ "${1:0:1}" = '-' ] ; then
-  set -- start-foreground $@
-fi
-
 # if request foreground mode
-if [ "$1" == "start-foreground" ] ; then
-  ensemble=false
+if [ "${1:0:1}" = '-' ] || [ "$1" == "start-foreground" ] ; then
   while [[ $1 ]]
   do
     case "$1" in
-      -s | --server)
-        echo "server.$2" >> conf/zoo.cfg
-        shift 2
-        ensemble=true
-        ;;
       -id)
         echo $2 > data/myid
+        shift 2
+        ;;
+      -p | --property)
+        echo $2 >> conf/zoo.cfg
         shift 2
         ;;
       *)
@@ -27,11 +20,6 @@ if [ "$1" == "start-foreground" ] ; then
         ;;
     esac
   done
-
-  if $ensemble ; then
-    echo "initLimit=5" >> conf/zoo.cfg
-    echo "syncLimit=2" >> conf/zoo.cfg 
-  fi
 
   set -- su-exec zookeeper bin/zkServer.sh start-foreground
 fi
